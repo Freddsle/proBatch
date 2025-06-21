c362fe3c4723f11b101fbb7665189c05f471cf19
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2025-06-21 11:27:16 +0200

    minor fixes in colors - n_base and pallets

--- tests/testthat/test-colors_for_annotation.R
+++ tests/testthat/test-colors_for_annotation.R
@@ -48,3 +48,18 @@ test_that("sample_annotation_to_colors", {
   expect_equal(color_scheme$DateTime[[1]], "#A6611A")
   expect_equal(color_scheme$order[[1]], "#D01C8B")
 })
+
+test_that("color_list_to_df handles empty intersection", {
+  data(example_sample_annotation, package = "proBatch")
+
+  color_list <- list(AnotherFactor = c("red", "blue"))
+  expect_warning(
+    color_df <- proBatch:::color_list_to_df(color_list, example_sample_annotation),
+    "color list and sample annotation have different factors,
+            using only intersection in color scheme!"
+  )
+
+  expect_equal(nrow(color_df), nrow(example_sample_annotation))
+  expect_equal(ncol(color_df), 0)
+  expect_equal(rownames(color_df), example_sample_annotation$FullRunName)
+})
