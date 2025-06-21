24557db453083d372963f710a21c43aad02cdb64
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2025-06-21 13:52:38 +0200

    fixed all_of selection, fixed tests warnings, added transform_raw tests

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -130,6 +130,12 @@ create_peptide_annotation <- function(df_long,
             setdiff(names(df_long), protein_col)
         )
     }
+
+    # if feature_id_col is not in df_long, stop
+    if (!(feature_id_col %in% names(df_long))) {
+        stop(sprintf("Column %s is not in the data", feature_id_col))
+    }
+    
     peptide_annotation <- df_long %>%
         select(all_of(c(feature_id_col, protein_col))) %>%
         distinct()
