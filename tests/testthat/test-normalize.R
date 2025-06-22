30c03fb05aca02886ae0e34f51c568f8441f8f32
Yuliya Burankova (Freddsle) <burankova.y@gmail.com>
2025-06-22 23:18:37 +0200

    Add tests for normalize_data_dm to verify offset handling; include example_sample_annotation in hierarchical clustering test

--- tests/testthat/test-normalize.R
+++ tests/testthat/test-normalize.R
@@ -9,8 +9,6 @@ test_that("quantile_normalize", {
     expect_equal(quant_matrix[1:10], quant_normalized_matrix[1:10], ignore_attr = TRUE)
 })
 
-
-
 test_that("normalize_sample_medians", {
     data(example_proteome, package = "proBatch")
 
@@ -32,4 +30,16 @@ test_that("normalize_sample_medians", {
     expect_equal(median_centered_data$Intensity[1], 2728887.42, tolerance = 1e-2, ignore_attr = TRUE)
     expect_equal(median_centered_data$Intensity[549], 35191.95, tolerance = 1e-2, ignore_attr = TRUE)
     expect_equal(median_centered_data$preNorm_Intensity[549], 25479.8, ignore_attr = TRUE)
+})
+
+test_that("normalize_data_dm forwards offset to log transform", {
+    data(example_proteome_matrix, package = "proBatch")
+    mat <- example_proteome_matrix[1:5, 1:4]
+    res_quant <- normalize_data_dm(mat, normalize_func = "quantile", log_base = 2, offset = 0.5)
+    expected_quant <- quantile_normalize_dm(log_transform_dm(mat, log_base = 2, offset = 0.5))
+    expect_equal(res_quant, expected_quant, ignore_attr = TRUE)
+
+    res_median <- normalize_data_dm(mat, normalize_func = "medianCentering", log_base = 2, offset = 0.5)
+    expected_median <- normalize_sample_medians_dm(log_transform_dm(mat, log_base = 2, offset = 0.5))
+    expect_equal(res_median, expected_median, ignore_attr = TRUE)
 })
